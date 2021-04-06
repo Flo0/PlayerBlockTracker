ff8327b00d14cb619c8ed418aa6ee0e67a82603c
Flo0 <unconfigured@null.spigotmc.org>
2021-04-06 23:23:50 +0200

    Added API documentation

--- src/main/java/com/gestankbratwurst/playerblocktracker/PlayerBlockTracker.java
+++ src/main/java/com/gestankbratwurst/playerblocktracker/PlayerBlockTracker.java
@@ -28,18 +28,40 @@ public final class PlayerBlockTracker extends JavaPlugin {
     this.trackingManager.terminateCurrentlyLoadedWorlds();
   }
 
+  /**
+   * Returns true if the block is marked as tracked.
+   *
+   * @param block
+   * @return true if it is tracked false otherwise.
+   */
   public static boolean isTracked(final Block block) {
     return instance.trackingManager.isTracked(block);
   }
 
+  /**
+   * Manually tag a block as tracked. Use this if you want to track the creation of blocks from your own plugin.
+   *
+   * @param block
+   */
   public static void track(final Block block) {
     instance.trackingManager.track(block);
   }
 
+  /**
+   * Manually un-tag a block as tracked. Use this if you want to track the removal of blocks from your own plugin.
+   *
+   * @param block
+   */
   public static void unTrack(final Block block) {
     instance.trackingManager.unTrack(block);
   }
 
+  /**
+   * Manually move a tracked position to another one. Should not be used in bulk operations.
+   *
+   * @param from
+   * @param to
+   */
   public static void moveTrack(final Block from, final Block to) {
     instance.trackingManager.move(from, to);
   }
